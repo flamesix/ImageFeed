0b1fac72b8f9a3c640660b1d96b2573098c119f3
grinevich <grinevichyu@gmail.com>
2024-06-01 17:39:27 +0300

    initial project

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Юрий Гриневич on 01.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
